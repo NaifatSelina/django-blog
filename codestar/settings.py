cdf08dffdfb035cf94b05386bb5d87716cf31134
NaifatSelina <naifatselina@icloud.com>
2023-03-27 15:11:11 +0000

    Add bootsrap javascript alerts

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"):
     import env
 
@@ -59,6 +60,15 @@ LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
